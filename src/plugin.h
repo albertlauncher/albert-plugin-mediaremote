d435eb65078691e1a081a961171ad8ce3a44b642
Manuel Schneider <manuelschneid3r@gmail.com>
2024-03-06 23:02:16 +0100

    [mpris:3.1]
    * 0.24
    * Move v3 to archive
    * Rewrite using xml interface files

--- src/plugin.h
+++ src/plugin.h
@@ -1,21 +1,27 @@
 // Copyright (c) 2017-2024 Manuel Schneider
 
 #pragma once
-#include "albert/extension/queryhandler/globalqueryhandler.h"
-#include "albert/plugin.h"
+#include <albert/extensionplugin.h>
+#include <albert/globalqueryhandler.h>
 
-class Plugin : public albert::plugin::ExtensionPlugin,
+class Plugin : public albert::ExtensionPlugin,
                public albert::GlobalQueryHandler
 {
-    Q_OBJECT ALBERT_PLUGIN
+    ALBERT_PLUGIN
+
 public:
+
     Plugin();
     ~Plugin();
 
-    std::vector<albert::RankItem> handleGlobalQuery(const GlobalQuery *) const override;
+    std::vector<albert::RankItem> handleGlobalQuery(const albert::Query *) const override;
     QWidget *buildConfigWidget() override;
 
 private:
+
+    void serviceOwnerChanged(const QString&, const QString&, const QString&);
+
     struct Private;
     std::unique_ptr<Private> d;
+
 };
